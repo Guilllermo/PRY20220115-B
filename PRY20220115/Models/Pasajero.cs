62b637d5e470c41bbb5217cecfb7369e0b0e13b2
Renzo <U201819619@upc.edu.pe>
2022-07-01 11:45:27 -0500

    feat: class pasajero done

---
+++ PRY20220115/Models/Pasajero.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRY20220115.Models
+{
+    public class Pasajero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Mail { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        public string? PhoneNumber { get; set; }
+    }
+}
